e05bcb2ddd5b9095eff3145e76d6252e4b17ddbd
Tahir Duzyol <93155740+tahirduzyol@users.noreply.github.com>
2022-03-10 09:25:26 -0500

    Initial commit

---
+++ src/test/java/com/vytrack/tests/US70.java
@@ -0,0 +1,44 @@
+package com.vytrack.tests;
+
+import com.vytrack.utilities.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class US70 {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod() {
+
+        driver = Driver.getDriver(ConfigurationReader.getProperty("browser"));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(ConfigurationReader.getProperty("env"));
+
+    }
+
+    @Test(priority = 1)
+    public void verify() {
+        //Go to : login page
+
+        //Calling login utility method to login
+
+        BrowserUtils.sleep(3);
+    }
+
+}
+
+/*
+
+User Story
+
+As a user, I want to view columns on the Vehicle models page.(web-table)
+
+*/
